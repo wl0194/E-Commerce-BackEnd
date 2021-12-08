52aab8567bb51098bd30207265a4045a4502c179
Amy Hwang <728amyh@gmail.com>
2021-12-08 15:00:18 -0500

    added product and tag routes

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # E-Commerce-BackEnd
+
+## Description
+This application configures a working Express.js API to use Sequelize to interact with a MySQL database.
+
+## Installation
+Node.js, npm and Mysql must be installed to use this application.
+
+## Demo
+
+
+* [video-demo]()
+
+## Links
+
+* [Github](https://github.com/wl0194)
+
+## License
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,19 +4,42 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
-  // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findAll({
+      include:[
+        Category,
+        Tag,
+      ]
+    });
+    res.status(200).json(productData);
+  } catch(err) {
+    res.status(500).json(err);
+  }
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findOne({
+     where: {
+       id: req.params.id,
+     },
+    include: [
+      Category,
+      Tag,
+    ],
+    });
+    res.status(200).json(productData);
+  } catch(err) {
+    res.status(500).json(err);
+  }
 });
 
 // create new product
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   /* req.body should look like this...
     {
       product_name: "Basketball",
@@ -89,8 +112,22 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+    if (!productData) {
+      res.status(404).json({ message: "No Product found with this id!"});
+      return;
+    }
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
